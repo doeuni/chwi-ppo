17e112be19df0266ac69af3fdc35c8f2c59c88af
Doeun Kim <143738200+doeuni@users.noreply.github.com>
2024-11-20 21:40:28 +0900

    Title: 평균점수 구하기, Time: 185ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241120/평균점수 구하기/print-average.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let tmp = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let sum = 0 
+// let avg = sum =+ tmp
+tmp.forEach(num => sum += num )
+// console.log(sum)
+let avg2 = sum / 8
+console.log(avg2)
