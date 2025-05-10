bfba5048d4725d4ffe7b3abd91d3afa4c08d92f9
Doeun Kim <143738200+doeuni@users.noreply.github.com>
2025-05-10 21:01:10 +0900

    Title: 반복 출력하기, Time: 141ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250510/반복 출력하기/repeated-output.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void temp(int n) {
+        for(int i= 0; i<n; i++)
+            System.out.println("12345^&*()_");
+        
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        temp(n);
+        // Please write your code here.
+    }
+}
